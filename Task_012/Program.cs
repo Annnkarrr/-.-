a06919c2cc43526bd14d81041686bac68689c52b
Карякина Настя <nakarykina04@mail.ru>
2024-02-08 21:42:28 +0300

    Добавила решение 2

---
+++ Task_012/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+int[] array ={1, 5, 8, 0, 9, 52};
+PrintArray(array);
+Console.Write($" => ");
+PrintReverseArray(array, 0);
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == array.Length - 1)
+        {
+            Console.Write(array[i]);
+
+        }
+        else
+        {
+            Console.Write(array[i] + " ");
+        }
+
+    }
+    Console.Write("]");
+}
+
+void PrintReverseArray(int[] array, int i)
+{
+    if (i == array.Length - 1)
+    {
+        Console.Write(array[i] + " ");
+        return;
+    }
+
+    PrintReverseArray(array, i + 1);
+    Console.Write(array[i] + " ");
+}
+
+
